3338cb032e09a4ef7aee0fd067e63ef3c45dcb5e
JohannesRonndahl <149588320+JohannesRonndahl@users.noreply.github.com>
2023-11-10 10:43:24 +0100

    WS4 Hemma tjuvstart

--- My project/Assets/Scripts/MainMenu.cs
+++ My project/Assets/Scripts/MainMenu.cs
@@ -6,14 +6,41 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   
+
+    public GameObject PauseMenu;
+
+
     public void PlayGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        
+        Time.timeScale = 1;
     
     }
 
+    public void MainMenuMenu()
+    {
+        SceneManager.LoadSceneAsync("Menu", LoadSceneMode.Single);
+        
+    }
+
+    public void Pause() 
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0;
+      
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Pause();
+    }
+
+    public void Continue()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
 
     public void QuitGame ()
     {
